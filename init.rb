2852489dd4d30bd91c65303c3d46bb3d16916f1f
Tobias Fischer <tobias.carl.fischer@gmail.com>
2020-01-15 20:35:25 +0100

    version 1.0.1

--- init.rb
+++ init.rb
@@ -4,10 +4,10 @@ require_dependency 'google_analytics_hooks'
 
 Redmine::Plugin.register :google_analytics_plugin do
   name 'Google Analytics plugin'
-  author 'Eric Davis & Raphael Kallensee'
+  author 'Eric Davis, Raphael Kallensee, Tobias Fischer'
   description 'Redmine plugin to insert a Google Analytics (or e.g. Piwik) tracking code into Redmine based on user roles.'
-  url 'https://github.com/rkallensee/redmine-google-analytics-plugin'
-  version '1.0.0'
+  url 'https://github.com/paginagmbh/redmine-google-analytics-plugin'
+  version '1.0.1'
 
   requires_redmine :version_or_higher => '2.0.0'
 
@@ -19,4 +19,3 @@ Redmine::Plugin.register :google_analytics_plugin do
   }, :partial => 'google_analytics_settings'
 
 end
-
